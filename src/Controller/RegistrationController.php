7e320bd4df32f1bc274b525a5efdfcfbb92f5a61
AurelJAX <aurelien.weggen@gmail.com>
2021-10-13 09:01:27 +0200

    Debut Bootstrap

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_register")
+     * @Route("/admin/register", name="app_register")
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {

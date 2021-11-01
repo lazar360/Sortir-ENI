f145c99490ef7a03013860753f8d78456df02e14
NicolasGautier <nicolas.gautier35135@gmail.com>
2021-11-01 11:38:49 +0100

    fixtures etat

--- src/DataFixtures/SortieFixtures.php
+++ src/DataFixtures/SortieFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Etat;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,25 @@ class SortieFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $etatCree = new Etat();
+        $etatCree->setLibelle("créée");
+        $manager->persist($etatCree);
+
+        $etatPubliee = new Etat();
+        $etatPubliee->setLibelle("publiée");
+        $manager->persist($etatPubliee);
+
+        $etatCloturee = new Etat();
+        $etatCloturee->setLibelle("clôturée");
+        $manager->persist($etatCloturee);
+
+        $etatEnCours = new Etat();
+        $etatEnCours->setLibelle("en cours");
+        $manager->persist($etatEnCours);
+
+        $etatArchivee = new Etat();
+        $etatArchivee->setLibelle("archivée");
+        $manager->persist($etatArchivee);
 
         $manager->flush();
     }
